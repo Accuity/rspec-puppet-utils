e6a283529369d8551e359faa171836f48222b7c5
Tom Poulton <tom.poulton@rbi.co.uk>
2014-03-11 18:09:06 -0500

    Fixed require paths

--- lib/rspec-puppet-utils.rb
+++ lib/rspec-puppet-utils.rb
@@ -1,9 +1,9 @@
 require 'rspec'
 require 'mocha'
-require 'lib/mock_function'
-require 'lib/template_harness'
-require 'lib/hieradata/validator'
-require 'lib/hieradata/yaml_validator'
+require 'mock_function'
+require 'template_harness'
+require 'hieradata/validator'
+require 'hieradata/yaml_validator'
 
 RSpec.configure do |c|
   c.mock_with :mocha
